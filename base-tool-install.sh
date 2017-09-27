808becc10c2c5d625cf487072c431e2057e683d8
marco gablowsky <m.gablowsky@googlemail.com>
2017-09-27 21:30:16 +0200

    configured i3blocks; fixed tray icons by configuring primary output (xrandr)

--- base-tool-install.sh
+++ base-tool-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install htop curl git maven vim tmux gtypist zsh texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
+sudo apt-get update && sudo apt-get install htop curl git maven vim tmux gtypist zsh lm-sensors texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
 
 # setup zsh
 echo "Setting zsh as default shell"
