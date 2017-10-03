d1813d4bcf16ea8c9f86d50f1806a350efdf763a
marco gablowsky <m.gablowsky@googlemail.com>
2017-10-03 20:37:02 +0200

    added vim 8 ppa

--- base-tool-install.sh
+++ base-tool-install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install htop curl git maven vim tmux gtypist zsh lm-sensors texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
+# add ppa for backport of vim 8
+sudo add-apt-repository ppa:jonathonf/vim
+sudo apt-get update && sudo apt-get install htop curl git vim tmux gtypist zsh lm-sensors texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
 
 # setup zsh
 echo "Setting zsh as default shell"
