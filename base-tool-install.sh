368236483c31045f013c9c0b7eb979411c7c9f49
marco gablowsky <m.gablowsky@googlemail.com>
2017-11-05 18:01:24 +0100

    added ranger to base setup script

--- base-tool-install.sh
+++ base-tool-install.sh
@@ -2,7 +2,7 @@
 
 # add ppa for backport of vim 8
 sudo add-apt-repository ppa:jonathonf/vim
-sudo apt-get update && sudo apt-get install htop curl git vim tmux gtypist zsh lm-sensors texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
+sudo apt-get update && sudo apt-get install htop curl git vim tmux gtypist zsh ranger lm-sensors texlive texlive-latex-extra texlive-lang-german cowsay cmatrix -y
 
 # setup zsh
 echo "Setting zsh as default shell"
